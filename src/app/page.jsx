46e9a5512cf844fa03b90666b827cab4d21fa9bc
Sarim <sarimrafiq100@gmail.com>
2023-10-24 13:34:48 +0500

    a

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,10 @@
+import Link from "next/link";
 import styles from "./homepage.module.css";
-
 export default function Home() {
-  return <div>Hello</div>;
+  return <div>
+    <Link href="/">
+      <hl>BLOG</hl>
+
+      The TechCrunch website The TechCrunch websiteThe TechCrunch websiteThe TechCrunch websiteThe TechCrunch websiteThe TechCrunch websiteThe TechCrunch websiteThe TechCrunch websiteThe TechCrunch websiteThe TechCrunch websiteThe TechCrunch websiteThe TechCrunch websiteThe TechCrunch websiteThe TechCrunch websiteThe TechCrunch websiteThe TechCrunch websiteThe TechCrunch websiteThe TechCrunch websiteThe TechCrunch websiteThe TechCrunch websiteThe TechCrunch websiteThe TechCrunch websiteThe TechCrunch website</Link>
+  </div>;
 }
