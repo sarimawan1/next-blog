8c83a6bd178bee9b343275133c40c065ca372f31
Sarim <sarimrafiq100@gmail.com>
2023-10-24 16:09:55 +0500

    c

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,19 +1,25 @@
+import Footer from '@/componenets/Footer/Footer'
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Navbar from '@/componenets/Navbar/Navbar'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['english'] })
 
 export const metadata = {
   title: 'Blog App',
-  description: 'The best blog app!',
+  description: 'My WEARIFY Brand!',
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="container">{children}
-          <div className="wrapper">{children}</div>
+        <div className="container">
+          <div className="wrapper">
+          <Navbar/>
+          {children}
+          <Footer/>
+          </div>
         </div>
       </body>
     </html>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,6 @@
 import Link from "next/link";
 import styles from "./homepage.module.css";
-import Navbar from "@/componenets/navbar/Navbar";
-import Footer from "@/componenets/footer/Footer";
 export default function Home() {
   return <div>
-    <Navbar/>
-    <Footer/>
   </div>;
 }
