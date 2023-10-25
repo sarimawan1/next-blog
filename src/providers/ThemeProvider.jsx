ad267148c5e52c9fecc577ec193fc7c4c024f14f
Sarim <sarimrafiq100@gmail.com>
2023-10-25 13:25:52 +0500

    j

--- src/providers/ThemeProvider.jsx
+++ src/providers/ThemeProvider.jsx
@@ -6,7 +6,7 @@ import React, {useContext, useState} from 'react'
 
 const ThemeProvider = ({children}) => {
 
-   // const {theme} = useContext(ThemeContext)
+    const {theme} = useContext(ThemeContext)
 
         return <div className={theme}>{children}</div>
     
