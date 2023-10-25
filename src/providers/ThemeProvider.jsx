9f816c886347f7f32742533a1352056b65b838f2
Sarim <sarimrafiq100@gmail.com>
2023-10-25 13:23:34 +0500

    h

--- src/providers/ThemeProvider.jsx
+++ src/providers/ThemeProvider.jsx
@@ -7,15 +7,8 @@ import React, {useContext, useState} from 'react'
 const ThemeProvider = ({children}) => {
 
    // const {theme} = useContext(ThemeContext)
-    const {mounted, setMounted} = useState(false);
 
-    useEffect(() => {
-        setMounted(true);
-    }, [setMounted]);
-
-    if (mounted) {
         return <div className={theme}>{children}</div>
-    }
     
 };
 
